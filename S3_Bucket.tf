3341b3ab36f3984bf5dbcd2b6c900f2c1a2ad47c
Sureshle <47473530+Sureshle@users.noreply.github.com>
2020-10-24 14:19:28 +0530

    Create S3 bucket using terraform script

---
+++ S3_Bucket.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+  region 		= "ap-south-1"
+}
+
+# s3 Bucket with Website settings
+resource "aws_s3_bucket" "site_bucket" {
+  bucket = "${var.site_name}"
+  acl = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
