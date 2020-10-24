ffc2ba1fa7d2d29ce88b99a82ec854564d160bab
Sureshle <47473530+Sureshle@users.noreply.github.com>
2020-10-24 14:43:07 +0530

    Update S3_Bucket.tf

--- S3_Bucket.tf
+++ S3_Bucket.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 # s3 Bucket with Website settings
 resource "aws_s3_bucket" "site_bucket" {
-  bucket = "futurefocus"
+  bucket = "futurefocus_demo"
   acl = "public-read"
   website {
     index_document = "index.html"
